6e5379a8b8458b446c58cd77f47637c510c21c50
unknown <abdulfahadgo566@gmail.com>
2022-07-11 00:55:02 +0800

    Setup login and register auth

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,12 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function () {
-    return "New Laravel Application";
-});
+Auth::routes();
 
-Route::get('/goal', function () {
-    return "this time we'll officially create a water billing system";
-});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
