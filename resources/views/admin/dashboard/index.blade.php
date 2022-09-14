e460cf9beffec4d15638d157fd0afefc2906209b
abby2727 <abdulfahadgo566@gmail.com>
2022-09-14 23:32:32 +0800

    Update project

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+        <h1 class="h3 mb-0 text-gray-800">My Dashboard</h1>
     </div>
 
     <div class="row">
