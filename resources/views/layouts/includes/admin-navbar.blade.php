de55eabf7d0906a65c7b93e3afdf48229b337947
Abdul Pangandaman <114658022+Abdul2727@users.noreply.github.com>
2022-10-18 03:40:43 +0800

    Admin-navbar minor changes

--- resources/views/layouts/includes/admin-navbar.blade.php
+++ resources/views/layouts/includes/admin-navbar.blade.php
@@ -13,7 +13,7 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Abdul Acibo Pangandaman</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Cheramie Etabes</span>
                 <img class="img-profile rounded-circle"
                     src="https://visualpharm.com/assets/314/Admin-595b40b65ba036ed117d36fe.svg">
             </a>
